61284e55643f50606b30c1c5cee7381d8441898f
Tre Logan <tr3.logan@gmail.com>
2018-07-26 01:26:57 -0400

    change ChatColor import to bukkit instead of bungee

--- src/ttdev/genwand/InventoryManager.java
+++ src/ttdev/genwand/InventoryManager.java
@@ -1,10 +1,10 @@
 package ttdev.genwand;
 
+
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-
-import net.md_5.bungee.api.ChatColor;
 import ttdev.api.bukkit.Manager;
 import ttdev.api.user.inventory.AInventory;
 import ttdev.api.user.inventory.events.inventoryclick.InventoryClick;
